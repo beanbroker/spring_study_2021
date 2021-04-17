96effb5c3fe64e164e929b346d10100b49efa0a5
beanbroker <88p7h21@gmail.com>
2021-04-18 02:41:15 +0900

    33

---
+++ src/main/java/com/pkj/boker/demo/test/TestConfig.java
@@ -0,0 +1,16 @@
+package com.pkj.boker.demo.test;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class TestConfig {
+
+    @Bean
+    public List<String> abc(){
+        return Arrays.asList("a","b","c");
+    }
+}
---
+++ src/main/java/com/pkj/boker/demo/test/TestController.java
@@ -0,0 +1,32 @@
+package com.pkj.boker.demo.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+  @Autowired
+  private List<String> abc;
+
+  @GetMapping
+  public List<String> abc() {
+
+    ApplicationContext context = new AnnotationConfigApplicationContext(TestConfig.class);
+    List abc = context.getBean("abc", List.class);
+    return abc;
+  }
+
+  @GetMapping("/test")
+  public List<String> abcList() {
+    return abc;
+  }
+}
